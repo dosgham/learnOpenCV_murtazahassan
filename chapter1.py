1b4d4d9f3a309ffbd884d11dbefd95d4fc189f6e
dosgham <78833161+dosgham@users.noreply.github.com>
2021-05-20 23:01:21 -0400

    lesson1

---
+++ chapter1.py
@@ -0,0 +1,23 @@
+import cv2
+'''
+print("Package Imported")
+img = cv2.imread("Resources/lenna.jpg")
+cv2.imshow('Output',img)
+cv2.waitKey(100)
+cap = cv2.VideoCapture(0)
+cap.set(10,100)
+while True:
+    success,img = cap.read()
+    cv2.imshow('Video',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        '''
+img = cv2.imread('Resources/lenna.jpg')
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+
+
+cv2.imshow('Gray Image',imgGray)
+cv2.imshow('Blur Image',imgBlur)
+cv2.waitKey(0)
+
