d2c6d81964e6e8aae59b1e3baad02e635eed5720
dosgham <78833161+dosgham@users.noreply.github.com>
2021-05-30 06:46:14 -0400

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -4,6 +4,9 @@ print("Package Imported")
 img = cv2.imread("Resources/lenna.jpg")
 cv2.imshow('Output',img)
 cv2.waitKey(100)
+'''
+
+
 cap = cv2.VideoCapture(0)
 cap.set(10,100)
 while True:
@@ -11,13 +14,20 @@ while True:
     cv2.imshow('Video',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-        '''
+
+
+
+'''
 img = cv2.imread('Resources/lenna.jpg')
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imgCanny = cv2.Canny(img,100,100)
 
 
 cv2.imshow('Gray Image',imgGray)
 cv2.imshow('Blur Image',imgBlur)
+cv2.imshow('Canny Image',imgCanny)
 cv2.waitKey(0)
+'''
+
 
